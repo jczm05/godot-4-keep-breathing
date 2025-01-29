552297c5fea3bed8d5d6af410bee7ce5af89c1e1
jczm05 <juancarloszm05@gmail.com>
2025-01-29 12:02:17 +0100

    added change of levels

---
+++ scripts/next_level.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+var FILE_PATH = "res://scenes/level_"
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		var next_level_number = get_tree().current_scene.scene_file_path.to_int()+1
+		var next_level_path = FILE_PATH + str(next_level_number) + ".tscn"
+		print(next_level_path)
+		get_tree().change_scene_to_file(next_level_path)
