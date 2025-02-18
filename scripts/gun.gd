354d0885819a218ba405795cd8bb424a5085a7ed
jczm05 <juancarloszm05@gmail.com>
2025-02-18 16:37:34 +0100

    Gun made

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -1,19 +1,35 @@
 extends Node2D
-const BULLET = preload("res://scenes/bullet.tscn")
+var bullet_speed = 700
+var bullet = preload("res://scenes/bullet.tscn")
+@onready var bullet_spawn: Marker2D = $bullet_spawn
+@onready var shoot_speed_timer: Timer = $shootSpeedTimer
+var canShoot = true
+var shootSpeed = 2.0
+@onready var gun: Sprite2D = $"."
 
-
-
- 
-@onready var muzzle: Node2D = $"."
- 
- 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
- 
- 
+	if get_global_mouse_position().x > global_position.x:
+		gun.flip_v = false
+	else:
+		gun.flip_v = true
+	
 	if Input.is_action_just_pressed("shoot"):
-		var bullet_instance = BULLET.instantiate()
-		get_tree().root.add_child(bullet_instance)
-		bullet_instance.global_position = muzzle.global_position
-		bullet_instance.apply_impulse(Vector2(), Vector2(50000, 0))
-		bullet_instance.rotation = rotation
+		fire()
+
+
+func fire():
+	if canShoot:
+		canShoot = false
+		shoot_speed_timer.start()
+		var bullet_instance = bullet.instantiate()
+		bullet_instance.global_position = bullet_spawn.global_position
+		var mouse_pos = get_global_mouse_position()
+		var direction = (mouse_pos - bullet_spawn.global_position).normalized()
+		bullet_instance.rotation = direction.angle()
+		bullet_instance.apply_impulse(direction * bullet_speed)
+		get_tree().root.call_deferred("add_child", bullet_instance)
+		
+
+func _on_shoot_speed_timer_timeout() -> void:
+	canShoot = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @export var speed = 150
-var bullet_speed = 700
-var bullet = preload("res://scenes/bullet.tscn")
-@onready var bullet_spawn: Marker2D = $bullet_spawn
+@onready var gun: Sprite2D = $gun
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -13,36 +15,21 @@ func _physics_process(_delta):
 	get_input()
 	move_and_slide()
 	play_animations()
-	if Input.is_action_just_pressed("shoot"):
-		fire()
+		
 
 func play_animations():
 	if Input.is_action_pressed("move_left"):
 		animated_sprite.play("run_left")
+		gun.visible = true
 	if Input.is_action_pressed("move_right"):
 		animated_sprite.play("run_right")
+		gun.visible = true
 	if Input.is_action_pressed("move_up"):
 		animated_sprite.play("run_up")
+		gun.visible = false
 	if Input.is_action_pressed("move_down"):
 		animated_sprite.play("run_down")
+		gun.visible = true
 	if !(Input.get_vector("move_left", "move_right", "move_up", "move_down")):
 		animated_sprite.play("idle")
-
-func fire():
-	# Instanciar la bala
-	var bullet_instance = bullet.instantiate()
-	bullet_instance.global_position = bullet_spawn.global_position  # Posición de inicio de la bala
-	
-	# Obtener la dirección hacia el ratón
-	var mouse_pos = get_global_mouse_position()
-	var direction = (mouse_pos - bullet_spawn.global_position).normalized()  # Dirección normalizada hacia el ratón
-	
-	# Establecer la rotación de la bala para que apunte a la dirección
-	bullet_instance.rotation = direction.angle()
-	
-	
-	# Usar RigidBody2D para la física
-	bullet_instance.apply_impulse(direction * bullet_speed)
-	
-	# Agregar la bala a la escena
-	get_tree().root.call_deferred("add_child", bullet_instance)
+		gun.visible = true
