4621d93bd2e469474b509e05decbc5d8012d7960
jczm05 <juancarloszm05@gmail.com>
2025-02-21 18:53:22 +0100

    knife erased and music changed

--- scripts/audio_player.gd
+++ scripts/audio_player.gd
@@ -1,15 +1,13 @@
 extends Node
 
-var menu_music = load("res://music/1-MGS3MM.mp3")
-var level1_music = load("res://music/2-MGS3LVL.mp3")
-var level2_music = load("res://music/3-MGS3LVL2.mp3")
+var menu_music = load("res://music/soliloquy.mp3")
+var level1_music = load("res://music/wasteland_overdrive_looped.wav")
+var level2_music = load("res://music/fall_of_arcana_new_era_looped.wav")
 
 func _ready():
-	pass
+	$Music.connect("finished", Callable(self, "_on_Music_finished"))
 
 func play_menu_music():
-	#$Music.stream = menu_music
-	#$Music.play()
 	if $Music.stream == menu_music:
 		return
 	$Music.stream = menu_music
@@ -25,4 +23,7 @@ func play_level2_music():
 	if $Music.stream == level2_music:
 		return
 	$Music.stream = level2_music
+	$Music.play()
+
+func _on_Music_finished():
 	$Music.play()
