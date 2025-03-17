7ff27a0ffd507041dbc71c13e6e6e815bc57e46b
jczm05 <juancarloszm05@gmail.com>
2025-03-17 16:40:18 +0100

    added bidon and ammo object fully working

--- scripts/ammo_item.gd
+++ scripts/ammo_item.gd
@@ -2,4 +2,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
+		var weapons = get_tree().get_nodes_in_group("Gun") # Obtener nodos del grupo "Weapons"
+		for gun in weapons:
+			gun.set_reserve_ammo(gun.reserve_ammo + 5) # Sumar 5 balas
 		queue_free()
--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -3,5 +3,5 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	Gui.hide()
 	MusicPlayer.play_level1_music()
