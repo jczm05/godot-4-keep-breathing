905fdd57b0e8e637601c959a44ecce19197d32d5
jczm05 <juancarloszm05@gmail.com>
2025-05-10 10:57:26 +0200

    cleaning

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -3,20 +3,14 @@ class_name Bullet
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 @onready var point_light_2d: PointLight2D = $PointLight2D
 
-
-#Variable for keeping track of it's velocity        
+	 
 var velocity:Vector2    
 var has_hit = false
 
-#Set the velocity of the bullet  
-#Call this right after creating the bullet to make it start moving
 func launch(direction:Vector2, speed:float):    
 	velocity = direction * speed    
 
-#This is automatically called every physics update.
 func _physics_process(delta):  
-	#Move the bullet using it's previously defined velocity  
-	#And save any collisions that may happen.
 	if has_hit:
 		return
 	var collision = move_and_collide(velocity * delta)
